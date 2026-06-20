dbe40621198e223cace216ff2186de5002828022
badarikajuluri-ronaldo <badarikajuluri@gmail.com>
2026-06-20 11:24:22 +0530

    Create Hero.jsx

---
+++ Hero.jsx
@@ -0,0 +1,16 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="overlay">
+        <h1>CULTURA 2025</h1>
+        <p>The Ultimate College Cultural Fest</p>
+
+        <a href="#register">
+          <button>Register Now</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
