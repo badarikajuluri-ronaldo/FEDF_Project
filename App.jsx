1c17f879ea969f23ada35b7cb194ee016fedde46
badarikajuluri-ronaldo <badarikajuluri@gmail.com>
2026-06-20 11:22:01 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import Hero from "./components/Hero";
+import Events from "./components/Events";
+import Registration from "./components/Registration";
+import Footer from "./components/Footer";
+import "./styles/Fest.css";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Events />
+      <Registration />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
