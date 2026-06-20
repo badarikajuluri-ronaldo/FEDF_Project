38525db2ca86b8d9bb34669db6042c246e0beed5
badarikajuluri-ronaldo <badarikajuluri@gmail.com>
2026-06-20 11:23:42 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <h3>CULTURA 2025</h3>
+      <p>XYZ College, Bangalore</p>
+      <p>Email: cultura2025@xyz.edu</p>
+    </footer>
+  );
+}
+
+export default Footer;
